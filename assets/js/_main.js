775b1691c5272a5e0af8628cb035f44e89502ab6
sorooshsorkhani <sorooshsorkhani@users.noreply.github.com>
2023-11-30 23:09:17 -0500

    Dark mode, attempt 1

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -96,3 +96,16 @@ $(document).ready(function(){
   });
 
 });
+
+// Dark mode
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+});
+
+if (localStorage.getItem('darkMode') === 'true') {
+    document.body.classList.add('dark-mode');
+}
+
